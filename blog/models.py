757374ead62cdc057cd1e0fbffbe18ca05faa691
twilightrus <rysik.kopilov.12@mail.ru>
2018-02-15 17:08:16 +0300

    Change fields (Articles)

--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class Article(models.Model):
-    article_title = models.CharField(max_length=200)
-    article_image = models.URLField(max_length=300)
-    article_text = models.TextField(default="")
+    title = models.CharField(max_length=255, default="")
+    image = models.URLField(default="")
+    description = models.TextField(default="")
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
-        return self.article_title
+        return self.title
 
     def get_next_id(self):
 
