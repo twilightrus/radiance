06ac1b6954441afe93367674923c6a3f0f4e8749
twilightrus <rysik.kopilov.12@mail.ru>
2018-03-06 19:45:48 +0300

    Rename projectname in comments

--- radiance/urls.py
+++ radiance/urls.py
@@ -1,4 +1,4 @@
-"""hooli URL Configuration
+"""radiance URL Configuration
 
 
 The `urlpatterns` list routes URLs to views. For more information please see:
--- radiance/wsgi.py
+++ radiance/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hooli project.
+WSGI config for radiance project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
